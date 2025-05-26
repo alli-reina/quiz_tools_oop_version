a054a5a56a2efea2f342ef40fcf78b107c0a7d3a
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-05-26 21:06:40 +0800

    Create text_wrapper.py
    Create TextWrapper class to wrap long text lines to fit within screen width.

---
+++ quiz_game_files/text_wrapper.py
@@ -0,0 +1,14 @@
+class TextWrapper:
+    def wrap_text_to_fit(self, text, font, max_width):
+        words = text.split(" ")
+        lines = []
+        current_line = ""
+        for word in words:
+            test_line = f"{current_line} {word}".strip()
+            if font.size(test_line)[0] <= max_width:
+                current_line = test_line
+            else:
+                lines.append(current_line)
+                current_line = word
+        lines.append(current_line)
+        return lines
